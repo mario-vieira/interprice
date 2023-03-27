2e347d6a81d007926e88eaa23b93c9fc8716a430
Mario Vieira <mario@securelogin.nu>
2023-03-27 10:29:40 +0100

    Components for single toggle and multiple toggle

---
+++ src/components/MultiToggle.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="btn-group" role="group">
+        <div class="btn-container" v-for="option in dataOptions" :key="option.value">
+            <input type="checkbox" class="btn-check" :id="option.value" autocomplete="off" :checked="option.checked" @change="onChange(option)">
+            <label class="btn btn-outline-primary" :for="option.value"> {{ option.title }} </label>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "MultiToggle",
+    props: {
+        options: {
+            type: Array,
+            required: true
+        },
+    },
+    data() {
+        return {
+            dataOptions: this.options
+        }
+    },
+    methods: {
+        onChange(option) {
+            option.checked = !option.checked;
+            let optionsChecked = [];
+            this.options.forEach(option => {
+                if (option.checked) {
+                    optionsChecked.push(option.value);
+                }
+            })
+            this.$emit('change', optionsChecked)
+        }
+    },
+    watch: {
+        options() {
+            this.dataOptions = this.options;
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/SingleToggle.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
+        <div class="btn-container" v-for="option in options" :key="option.value" >
+            <input type="radio" class="btn-check" :name="name" :id="option.title" autocomplete="off"
+                   :checked="option.checked" @change="$emit('change', option.value)"
+            >
+            <label class="btn btn-outline-primary" :for="option.title">{{option.title}}</label>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "SingleToggle",
+    props: {
+        name: {
+            type: String,
+            required: true
+        },
+        options: {
+            type: Array,
+            required: true
+        },
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
